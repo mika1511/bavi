bde300d0e2f383e1969dfbbcfdd8ba3babc3bb15
ish4n10 <91148819+ish4n10@users.noreply.github.com>
2023-06-04 17:40:03 +0530

    Update Orders.py

--- backend_bavi/models/Orders.py
+++ backend_bavi/models/Orders.py
@@ -13,7 +13,7 @@ class Order(Base):
     Pending = Column(BOOLEAN)
     PaymentOption = Column(String(200))
     OrderType = Column(String(100))
-    OrderTime =  Column(DateTime(timezone=True), server_default=text("CONVERT_TZ(NOW(), '+00:00', '+05:30')"))
+    OrderTime = Column(DateTime(timezone=True), server_default=func.now())
     
     def __init__(self, FirstName, ServiceName, TotalPrice, PhoneNumber, Pending, PaymentOption, OrderType):
         self.FirstName = FirstName
